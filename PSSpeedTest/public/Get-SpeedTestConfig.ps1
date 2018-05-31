c1c4cbf48ba26cffbdd13e5dfb567876bd38b408
Matthew Bobke <mcbobke@gmail.com>
2018-05-31 02:41:16 -0700

    Specify path

--- PSSpeedTest/public/Get-SpeedTestConfig.ps1
+++ PSSpeedTest/public/Get-SpeedTestConfig.ps1
@@ -15,7 +15,7 @@ function Get-SpeedTestConfig {
     Param()
 
     try {
-        $config = Get-Content "$($PSScriptRoot | Split-Path -Parent)\config.json"
+        $config = Get-Content -Path "$($PSScriptRoot | Split-Path -Parent)\config.json"
         return $config | ConvertFrom-Json
     }
     catch {
