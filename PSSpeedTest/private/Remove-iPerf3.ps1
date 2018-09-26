a0c20e3c32c47a96906f3a39cc0e0e60e078ef7e
Matthew Bobke <mcbobke@gmail.com>
2018-09-25 17:13:58 -0700

    Remove-iPerf3 function written

--- PSSpeedTest/private/Remove-iPerf3.ps1
+++ PSSpeedTest/private/Remove-iPerf3.ps1
@@ -15,4 +15,10 @@ function Remove-iPerf3 {
 
     )
 
+    try {
+        Get-Package -Name 'iperf3' -ProviderName 'ChocolateyGet' | Uninstall-Package
+    }
+    catch {
+        Write-Verbose -Message "Package 'iperf3' not found as installed by ChocolateyGet provider - no action taken."
+    }
 }
