727dbf40b85616f34204ad138b86522cb2a70ed6
Matthew Bobke <mcbobke@gmail.com>
2018-12-04 21:56:59 -0800

    Removal works on local machine
    Working to begin deprecating support for remote server setup/removal due to large amount of variables that can cause errors. Setup is best done by installing the module on the server itself and running the command locally or over a remote terminal.

--- PSSpeedTest/public/Remove-SpeedTestServer.ps1
+++ PSSpeedTest/public/Remove-SpeedTestServer.ps1
@@ -43,99 +43,31 @@ function Remove-SpeedTestServer {
         throw "You are not running as administrator. Please re-run this function after opening PowerShell as administrator."
     }
 
-    if (!($ComputerName)) {
-        Write-Output "Decommissioning local iPerf3 server."
+    Write-Output "Decommissioning local iPerf3 server."
 
-        # Remove scheduled task before stopping process to prevent auto-trigger
-        Remove-iPerf3Task
-        Write-Verbose -Message "Stopping iPerf3 process."
-        Get-Process -Name 'iperf3' | Stop-Process
-        Remove-iPerf3Port
-        Remove-iPerf3
+    # Remove scheduled task before stopping process to prevent auto-trigger
+    Remove-iPerf3Task
+    Write-Verbose -Message "Stopping iPerf3 process."
+    Get-Process -Name 'iperf3' | Stop-Process
+    Remove-iPerf3Port
+    Remove-iPerf3
 
-        $timeoutTimer = [Diagnostics.Stopwatch]::StartNew()
-        $processTest = $false
-        while ($timeoutTimer.Elapsed.TotalSeconds -lt $timeout) {
-            $getProcessResult = Get-Process -Name 'iperf3' -ErrorAction 'SilentlyContinue'
-            if (!$getProcessResult) {
-                Write-Verbose -Message "iPerf3 process does not exist."
-                $processTest = $true
-                break
-            }
-            else {
-                Start-Sleep -Seconds 3
-            }
-        }
-        $timeoutTimer.Stop()
-
-        if (!($processTest)) {
-            throw "iPerf3 process still running even though decommission was attempted. Timeout of $timeout seconds reached."
-        }
-    }
-    else {
-        if (!($Credential)) {
-            Write-Verbose -Message "Decommissioning iPerf3 server on domain computer $ComputerName."
-            try {
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3Task}
-                Write-Verbose -Message "Stopping iPerf3 process."
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Process -Name 'iperf3' | Stop-Process}
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3Port}
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3}
-            }
-            catch {
-                $PSCmdlet.ThrowTerminatingError($_)
-            }
-
-            $timeoutTimer = [Diagnostics.Stopwatch]::StartNew()
-            $processTest = $false
-            while ($timeoutTimer.Elapsed.TotalSeconds -lt $timeout) {
-                $getProcessResult = Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Process -Name 'iperf3' -ErrorAction 'SilentlyContinue'}
-                if (!$getProcessResult) {
-                    Write-Verbose -Message "iPerf3 process does not exist on $ComputerName."
-                    $processTest = $true
-                    break
-                }
-                else {
-                    Start-Sleep -Seconds 3
-                }
-            }
-            $timeoutTimer.Stop()
-
-            if (!($processTest)) {
-                throw "iPerf3 process still running even though decommission was attempted on computer $ComputerName. Timeout of $timeout seconds reached."
-            }
+    $timeoutTimer = [Diagnostics.Stopwatch]::StartNew()
+    $processTest = $false
+    while ($timeoutTimer.Elapsed.TotalSeconds -lt $timeout) {
+        $getProcessResult = Get-Process -Name 'iperf3' -ErrorAction 'SilentlyContinue'
+        if (!$getProcessResult) {
+            Write-Verbose -Message "iPerf3 process does not exist."
+            $processTest = $true
+            break
         }
         else {
-            Write-Verbose -Message "Decommissioning iPerf3 server on domain computer $ComputerName with credential $Credential."
-            try {
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3Task} -Credential $Credential
-                Write-Verbose -Message "Stopping iPerf3 process."
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Process -Name 'iperf3' | Stop-Process} -Credential $Credential
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3Port} -Credential $Credential
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock ${Function:Remove-iPerf3} -Credential $Credential
-            }
-            catch {
-                $PSCmdlet.ThrowTerminatingError($_)
-            }
-
-            $timeoutTimer = [Diagnostics.Stopwatch]::StartNew()
-            $processTest = $false
-            while ($timeoutTimer.Elapsed.TotalSeconds -lt $timeout) {
-                $getProcessResult = Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-Process -Name 'iperf3' -ErrorAction 'SilentlyContinue'} -Credential $Credential
-                if ($getProcessResult) {
-                    Write-Verbose -Message "iPerf3 process does not exist on $ComputerName."
-                    $processTest = $true
-                    break
-                }
-                else {
-                    Start-Sleep -Seconds 3
-                }
-            }
-            $timeoutTimer.Stop()
-
-            if (!($processTest)) {
-                throw "iPerf3 process still running even though decommission was attempted on computer $ComputerName with credential $Credential. Timeout of $timeout seconds reached."
-            }
+            Start-Sleep -Seconds 3
         }
+    }
+    $timeoutTimer.Stop()
+
+    if (!($processTest)) {
+        throw "iPerf3 process still running even though decommission was attempted. Timeout of $timeout seconds reached."
     }
 }
