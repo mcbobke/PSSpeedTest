9c0aead40269097d95914f8749fb5f5c731003e0
Matthew Bobke <mcbobke@gmail.com>
2019-05-08 13:17:13 -0700

    Write-Host removed
    Breaking change as $PassThru parameter now removed in two cases

--- PSSpeedTest/public/Get-SpeedTestConfig.ps1
+++ PSSpeedTest/public/Get-SpeedTestConfig.ps1
@@ -15,24 +15,20 @@
 
 function Get-SpeedTestConfig {
     [CmdletBinding()]
-    Param(
-        [Switch]
-        $PassThru
-    )
+    Param()
 
     try {
         Write-Verbose -Message "Getting content of config.json and returning as a PSCustomObject."
         $config = Get-Content -Path "$($PSScriptRoot | Split-Path -Parent)\config.json" -ErrorAction "Stop" | ConvertFrom-Json
 
-        if ($PassThru){
-            return $config
+        $config = [PSCustomObject] @{
+            DefaultInternetServer = $config.defaultInternetServer.defaultServer;
+            DefaultInternetPort = $config.defaultInternetServer.defaultPort;
+            DefaultLocalServer = $config.defaultLocalServer.defaultServer;
+            DefaultLocalPort = $config.defaultLocalServer.defaultPort;
         }
-        else {
-            Write-Host "Internet Server: $($config.defaultInternetServer.defaultServer)"
-            Write-Host "Internet Port: $($config.defaultInternetServer.defaultPort)"
-            Write-Host "Local Server: $($config.defaultLocalServer.defaultServer)"
-            Write-Host "Local Port: $($config.defaultLocalServer.defaultPort)"
-        }
+
+        return $config
     }
     catch {
         throw "Can't find the JSON configuration file. Use 'Set-SpeedTestConfig' to create one."
